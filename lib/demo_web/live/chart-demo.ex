071b64bcf2e739a2b20bad113ec457a60a39983c
rajeshpillai <pillai.rajesh@gmail.com>
2022-02-01 22:01:43 +0530

    Css fixes

--- lib/demo_web/live/chart-demo.ex
+++ lib/demo_web/live/chart-demo.ex
@@ -10,72 +10,12 @@ defmodule DemoWeb.ChartDemoLive do
     {:ok, assign(socket, :chart_data, get_chart_data())}
   end
 
-  @spec render(any) :: Phoenix.LiveView.Rendered.t()
-  def render(assigns) do
-    ~H"""
-      <div class="row">
-      <div class="card" style="width:20rem;">
-        <%= live_component @socket, SparkLine,
-              id: 1,
-              data: get_chart_data(),
-              subtitle: "In Progress",
-              title: "150",
-              fill_color: "green"
-        %>
-      </div>
-
-      <div class="card" style="width:20rem;">
-      <%= live_component @socket, SparkLine,
-            id: 2,
-            data: get_chart_data(),
-            subtitle: "Awaiting",
-            title: "40",
-            fill_color: "orange"
-           %>
-      </div>
-
-      <div class="card" style="width:20rem;">
-      <%= live_component @socket, SparkLine,
-            id: 3,
-            data: get_chart_data(),
-            subtitle: "On Hold",
-            title: "15",
-            fill_color: "red"
-            %>
-      </div>
-
-    </div>
-
-    <div class="row mt-5">
-      <div class="card" style="width:60rem;">
-
-          <%= live_component @socket, LineColumnChart,
-                id: 4,
-                data: get_chart_data() %>
-
-      </div>
-    </div>
-
-    <div class="row mt-5">
-      <div class="card" style="width:60rem;">
-
-          <%= live_component @socket, LineChart,
-                id: 4,
-                data: get_chart_data() %>
-
-      </div>
-    </div>
-    """
-  end
 
   defp get_chart_data do
     chart_data = %{
       labels: ["10-Jan-2022","11-Jan-2022","12-Jan-2022","13-Jan-2022","14-Jan-2022","15-Jan-2022","16-Jan-2022"],
       values: [50,30,45,33,35,89]
     }
-
     chart_data
   end
-
-
 end
